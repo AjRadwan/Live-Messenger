877babb18ffdf158545e677e57c8d5d0e6ab8d7a
Anowar <1hafizajr@gmail.com>
2021-08-17 11:48:13 +0600

    first commit

---
+++ javascript/signup.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".signup form"),
+continueBtn = form.querySelector(".button input"),
+errorText = form.querySelector(".error-text");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+}
+
+continueBtn.onclick = ()=>{
+    //Ajax code for validation and login
+    let xhr = new XMLHttpRequest();  //Creating xml Object
+    xhr.open("POST", "php/signup.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              let data = xhr.response;
+              if(data === "success"){
+                location.href="users.php";
+              }else{
+                errorText.style.display = "block";
+                errorText.textContent = data;
+              }
+          }
+      }
+    }
+
+     // sending the from data through ajax to php
+    let formData = new FormData(form);  //creating new form data Object
+    xhr.send(formData); // sending the form data to php from here
+}
